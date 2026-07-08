f6d7dfe6997c16c87b2585a189d39a2eb7c071a3
David Chicué <davegeroficial@gmail.com>
2026-07-08 13:19:40 -0500

    optimization for  js bundler

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/david_WP/',
   plugins: [react()],
+  build: { //performance solution for a huge javascript chunk 500kb(1.12 MB rendered) down to individual file division
+    rollupOptions: {
+      output: {
+        manualChunks(id){
+          if(id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')){
+            return 'react-core'
+          }
+
+          if(id.includes('node_modules')){
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
